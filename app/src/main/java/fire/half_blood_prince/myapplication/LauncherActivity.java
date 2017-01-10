6c28d12a19f6ff579acab031f20a7f7d618383d3
half-blood-prince <garu_sridhar@yahoo.com>
2017-01-10 14:23:12 +0530

    Inital Project Setup

---
+++ app/src/main/java/fire/half_blood_prince/myapplication/LauncherActivity.java
@@ -0,0 +1,12 @@
+package fire.half_blood_prince.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
